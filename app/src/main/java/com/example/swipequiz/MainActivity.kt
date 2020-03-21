0f72201fdbeb589eb653e4ecd4ec96ad9afe4f7a
Leander van Grinsven <leander550@live.nl>
2020-03-21 20:51:48 +0100

    ADDED - (in)correct strings for snackbar - Swiping left and right left = false, right = true - Question only disappears if the answer you gave is correct

--- app/src/main/java/com/example/swipequiz/MainActivity.kt
+++ app/src/main/java/com/example/swipequiz/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.swipequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,10 +27,42 @@ class MainActivity : AppCompatActivity() {
         rvQuestion.adapter = QuizAdapter
         rvQuestion.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
 
+        createItemTouchHelper().attachToRecyclerView(rvQuestion)
+
         for (i in Quiz.QUIZ_QUESTIONS.indices) {
             questions.add(Quiz(Quiz.QUIZ_QUESTIONS[i], Quiz.QUIZ_ANSWER[i]))
         }
         QuizAdapter.notifyDataSetChanged()
+    }
 
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                var answer = questions.get(position).answer
+                QuizAdapter.notifyDataSetChanged()
+                if (direction == ItemTouchHelper.RIGHT && answer == true || direction == ItemTouchHelper.LEFT && answer == false) {
+                    Snackbar.make(rvQuestion, getString(R.string.correct) , Snackbar.LENGTH_SHORT)
+                        .show()
+                    questions.removeAt(position)
+                }else{Snackbar.make(rvQuestion, getString(R.string.incorrect), Snackbar.LENGTH_SHORT)
+                    .show()}
+            }
+        }
+        return ItemTouchHelper(callback)
     }
 }
